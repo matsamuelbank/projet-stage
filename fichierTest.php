196b691cf82c0f81b5d12add3e83e27e5ead58ee
Matsamuelbank <bankouezimatsamuel@gmail.com>
2023-06-19 14:05:11 +0200

    fichier test contenant toute la partie texte du site , sans php sans image

---
+++ fichierTest.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="C:/wamp64/www/projetStageWordpress/wp-content/themes/theme1_samuel/style/style2.css">
+    <link rel="stylesheet" href="C:/wamp64/www/projetStageWordpress/wp-content/themes/theme1_samuel/style/lightbox.min.css">
+    
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!--contenu nav bar avant en php-->
+        </div>
+    </div>
+</nav>
+<div class="container">
+
+
+
+<div class="image-container">
+  <img src="" alt="Description de l'image" width="100%">
+</div>
+
+
+<section id="section1">
+    <div id="accueil">
+        <h2>Des terrasses en résine aux plages de piscines</h2>
+                        
+        <p>Chez Terrasse Résine by DÉCONOBLE, nous réalisons des projets autour des sols en résine et en granulat de marbre : terrasses, plages de piscines, cours et allées, création d'espaces extérieurs et intérieurs…</p>
+
+        <p>Que vous soyez p<strong>articuliers ou</strong> <strong>professionnels, v</strong>ous bénéficiez d'un <strong>accompagnement personnalisé. Nous trouvons</strong> les solutions les plus adaptées à votre environnement et vos espaces. Tous vos projets sont créés <strong>sur mesure </strong>et <strong>personnalisables </strong>selon vos besoins et surtout, vos envies !</p>
+
+        <br><br>
+
+        <p style="text-align: center;">Les avantages de la résine et du granulat de marbre : </p>
+        
+        <div id="containerF">
+            <img src="" alt="logo";>
+            <div id="containerFlament">
+                <p class="flamentR"> Personnalisable et sur-mesure</p>
+                <p class="flamentR">Couleur unie ou incrustation de forme et motif</p>
+                <p class="flamentR">De nombreux coloris</p>
+                <p class="flamentR">Revêtement drainant à base de résine polyuréthane stable aux UV et de granulats de marbre</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="section2">
+    <div id="réalisation">
+        <h2>Besoin d'inspiration ? Découvrez nos réalisations de sols, de terrasses et de plages de piscine</h2>
+        <p class="" style="color:#dd12c7;font-size:1.4rem"><strong>Des espaces à leur image pour des moments uniques</strong></p>
+
+        <!-- partie des images -->
+        <h3>Images sols</h3>
+        <div class="galerie">
+             <!--image-->
+        </div>
+
+        <h3>Images terrasse</h3>
+        <div class="galerie">
+            <!--image-->
+        </div>
+
+        <h3>Images piscine</h3>
+            <!--image-->
+        </div>
+
+
+        <p>Que pensent nos clients de leur sol en résine ?</p>
+        <!-- partie carrousel-->
+        <div class="container">
+            <div class="slide-container active">
+                <div class="slide">
+                    <i class="fas fa-quote-right icon"></i>
+                    <div class="user">
+                        <img src="" alt="">
+                        <div class="user-info">
+                            <h3>Hervé Charrier</h3>
+                            <div  class="stars">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="far fa-star"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <p class="text">Hervé Charrier recommande Deco Noble.</p>
+                </div>
+            </div>
+
+            <div class="slide-container">
+                <div class="slide">
+                    <i class="fas fa-quote-right icon"></i>
+                    <div class="user">
+                        <img src="" alt="">
+                        <div class="user-info">
+                            <h3>Brice AG</h3>
+                            <div  class="stars">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="far fa-star"></i>
+                            </div>
+                        </div>
+                    </div>
+                        
+                    <p class="text">Projet mené au top avec un délai et une qualité tenus.
+                    Loïc doit nous haïr pour le dessin…mais il aime relever des défis. Il est très doué. Merci à tous.</p>
+                </div>
+            </div>
+
+            <div class="slide-container">
+                <div class="slide">
+                    <i class="fas fa-quote-right icon"></i>
+                    <div class="user">
+                        <img src="" alt="">
+                        <div class="user-info">
+                            <h3>Thierry Muscat</h3>
+                            <div  class="stars">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="far fa-star"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <p class="text">Thierry Muscat recommande Deco Noble.</p>
+                </div>
+            </div>
+
+            <div id="next" class="fas fa-chevron-right" onclick="next()"></div>
+            <div id="prev" class="fas fa-chevron-left" onclick="prev()"></div>
+        </div>
+        <!-- partie des réservations-->
+
+        <h2>Un accompagnement personnalisé pour des projets sur-mesure</h2>
+        <div class="presentation-severine">     
+            <p class="presentation">
+                Bonjour, je suis Séverine de <span class="">Terrasse Résine</span> by <small>DÉCONOBLE</small>.<br>
+                Je vous accompagne et vous <b>conseille</b> dans la conception de votre <b>terrasse</b>, de votre <b>plage de piscine</b> ou de votre <b>sol en résine</b> et en <b>granulat de marbre</b>. Nous prenons le temps de définir ensemble les <b>caractéristiques techniques</b>, les <b>coloris</b> et le <b>design</b>. Mon <b>intérêt</b> pour la <b>décoration</b> et l'aménagement d'espace me permet d'apporter ma touche déco et <b>féminine</b> à votre projet, pour qu'il vous corresponde et vous plaise dans la durée. Je m'investis dans chaque projet à vos côtés afin qu'il soit pensé pour <b>vos futurs moments de vie</b> et de partage !
+            </p>
+            <img id="presentation-img" src="" alt="">
+            
+        </div>
+        
+    </div>
+
+</section>
+
+<section id="section3">
+    <div id="contact">
+        <h2>Vous avez un projet ? Terrasses, sols, allées, cours ; Prenons rendez-vous!</h2>
+        <p>Ensemble, nous concevons votre projet d'un point de vue technique et esthétique. Nous nous déplaçons et intervenons partout en Rhône-Alpes et Région PACA. Laissez-moi simplement vos coordonnées ou décrivez votre projet et je vous rappellerai !</p>
+
+        <!-- Partie du formulaire -->
+        <form method="POST" action="front-page.php">
+            <h1>Contactez-nous</h1>
+            <div class="separation"></div>
+            <div class="corps-formulaire">
+                <div class="gauche">
+                    <div class="groupe">
+                        <label>Objet </label>
+                        <input type="text" autocomplete="off" />
+                    </div>
+                    <div class="groupe">
+                        <label>Votre Nom</label>
+                        <input type="text" autocomplete="off" />
+                    </div>
+                    <div class="groupe">
+                        <label>Votre Prénom</label>
+                        <input type="text" autocomplete="off" />
+                    </div>
+                    <div class="groupe">
+                        <label>Votre adresse e-mail</label>
+                        <input type="text" autocomplete="off" />
+                    </div>
+                    <div class="groupe">
+                        <label>Votre téléphone</label>
+                        <input type="tel" autocomplete="off" />
+                    </div>
+                    
+                </div>
+
+                <div class="droite">
+                    <div class="groupe">
+                        <label>Message</label>
+                        <textarea placeholder="Saisissez ici..."></textarea>
+                    </div>
+                </div>
+            </div>
+
+            <div class="pied-formulaire" align="center">
+                <button>Envoyer le message</button>
+            </div>
+        </form>
+        
+        <p>Chez Terrasse Résine by Déconoble, nous vous accompagnons dans vos projets pour sublimer votre habitat et créer des espaces à votre image.</p>
+    </div>
+</section>
+
+
+<!-- Footer-->
+
+</div> <!-- ferméture div de la class container-->
+
+<div id="jaune"></div>
+<div id="bleu">
+    <p id="lieux" class="col">
+        Pour votre projet de terrasse en résine, retrouvez-nous à :
+        Lyon - Chambéry - Genas - Meyzieu - Chassieu - Colombier-Saugnieu - Miribel - Jonage - Bourgoin-Jailleu - Maubec - Décines - Saint-Cyr-au-Mont-d'Or - Écully
+    </p>
+    <p id="imgage" class="col"><img src="" alt=""></p>
+    <p id="numero" class="col">Séverine Blanc : <br> 06 20 43 05 06</p>
+</div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+               <!--contenu nav bar avant en php-->
+            </div>
+        </div>
+</nav>
+</body>
+</html>
