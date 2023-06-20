9fa343a00236680b8fc5f6d658464f87b1638d89
Matsamuelbank <bankouezimatsamuel@gmail.com>
2023-06-20 11:48:52 +0200

    modificationn du code qui est sur Gutenberg

--- fichierTest.php
+++ fichierTest.php
@@ -1,64 +1,62 @@
 <div class="container">
+    <div class="image-container">
+        <img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/deconoble-image-accueil.jpg" alt="Description de l'image" width="100%">
+    </div>
+    <section id="section1">
+        <div id="accueil">
+            <h2>Des terrasses en résine aux plages de piscines</h2>
+                            
+            <p>Chez Terrasse Résine by DÉCONOBLE, nous réalisons des projets autour des sols en résine et en granulat de marbre : terrasses, plages de piscines, cours et allées, création d'espaces extérieurs et intérieurs…</p>
 
-<div class="image-container">
-  <img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/deconoble-image-accueil.jpg" alt="Description de l'image" width="100%">
+            <p>Que vous soyez p<strong>articuliers ou</strong> <strong>professionnels, v</strong>ous bénéficiez d'un <strong>accompagnement personnalisé. Nous trouvons</strong> les solutions les plus adaptées à votre environnement et vos espaces. Tous vos projets sont créés <strong>sur mesure </strong>et <strong>personnalisables </strong>selon vos besoins et surtout, vos envies !</p>
 
-</div>
-<section id="section1">
-    <div id="accueil">
-        <h2>Des terrasses en résine aux plages de piscines</h2>
-                        
-        <p>Chez Terrasse Résine by DÉCONOBLE, nous réalisons des projets autour des sols en résine et en granulat de marbre : terrasses, plages de piscines, cours et allées, création d'espaces extérieurs et intérieurs…</p>
-
-        <p>Que vous soyez p<strong>articuliers ou</strong> <strong>professionnels, v</strong>ous bénéficiez d'un <strong>accompagnement personnalisé. Nous trouvons</strong> les solutions les plus adaptées à votre environnement et vos espaces. Tous vos projets sont créés <strong>sur mesure </strong>et <strong>personnalisables </strong>selon vos besoins et surtout, vos envies !</p>
-
-        <p style="text-align: center;">Les avantages de la résine et du granulat de marbre : </p>
-        
-        <div id="containerF">
-            <img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/flamand-rose.png" alt="logo";>
-            <div id="containerFlament">
-                <p class="flamentR"> Personnalisable et sur-mesure</p>
-                <p class="flamentR">Couleur unie ou incrustation de forme et motif</p>
-                <p class="flamentR">De nombreux coloris</p>
-                <p class="flamentR">Revêtement drainant à base de résine polyuréthane stable aux UV et de granulats de marbre</p>
+            <p style="text-align: center;">Les avantages de la résine et du granulat de marbre : </p>
+            
+            <div id="containerF">
+                <img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/flamand-rose.png" alt="logo";>
+                <div id="containerFlament">
+                    <p class="flamentR"> Personnalisable et sur-mesure</p>
+                    <p class="flamentR">Couleur unie ou incrustation de forme et motif</p>
+                    <p class="flamentR">De nombreux coloris</p>
+                    <p class="flamentR">Revêtement drainant à base de résine polyuréthane stable aux UV et de granulats de marbre</p>
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
-<section id="section2">
-    <div id="réalisation">
-        <h2>Besoin d'inspiration ? Découvrez nos réalisations de sols, de terrasses et de plages de piscine</h2>
-        <p class="" style="color:#dd12c7;font-size:1.4rem"><strong>Des espaces à leur image pour des moments uniques</strong></p>
+    <section id="section2">
+        <div id="réalisation">
+            <h2>Besoin d'inspiration ? Découvrez nos réalisations de sols, de terrasses et de plages de piscine</h2>
+            <p class="" style="color:#dd12c7;font-size:1.4rem"><strong>Des espaces à leur image pour des moments uniques</strong></p>
 
-        <!-- partie des images -->
-        <h3>Images sols</h3>
-        <div class="galerie">
-            <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img1-solG.jpg" ><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img1-sol" alt="img1-sol.jpg"></a>
-            <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img2-solG.jpg" data-lightbox="mygallery"><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img2-sol.jpg" alt="img-solG"></a>
-            <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img3-solG.jpg" data-lightbox="mygallery"><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img3-sol.jpg" alt="img3-solG"></a>
-            <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img4-solG.jpg" data-lightbox="mygallery"><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img4-sol.jpg" alt="img4-solG"></a>
+            <!-- partie des images -->
+            <h3>Images sols</h3>
+            <div class="galerie">
+                <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img1-solG.jpg" ><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img1-sol" alt="img1-sol.jpg"></a>
+                <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img2-solG.jpg" data-lightbox="mygallery"><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img2-sol.jpg" alt="img-solG"></a>
+                <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img3-solG.jpg" data-lightbox="mygallery"><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img3-sol.jpg" alt="img3-solG"></a>
+                <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img4-solG.jpg" data-lightbox="mygallery"><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img4-sol.jpg" alt="img4-solG"></a>
+            </div>
         </div>
-        
-<h3>Images terrasse</h3>
-<div class="galerie">
-    <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img1-terrasseG.jpg" data-lightbox="mygallery"><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img1-terrasse.jpg" alt="img1-terrasseG"></a>
-    <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img2-terrasseG.jpg" data-lightbox="mygallery"><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img2-terrasse.jpg" alt="img2-terrasseG"></a>
-    <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img3-terrasseG.jpg" data-lightbox="mygallery"><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img3-terrasse.jpg" alt="img3-terrasseG"></a>
-    <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img4-terrasseG.jpg" data-lightbox="mygallery"><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img4-terrasse.jpg" alt="img4-terrasseG"></a>
-</div>
+            
+        <h3>Images terrasse</h3>
+        <div class="galerie">
+            <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img1-terrasseG.jpg" data-lightbox="mygallery"><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img1-terrasse.jpg" alt="img1-terrasseG"></a>
+            <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img2-terrasseG.jpg" data-lightbox="mygallery"><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img2-terrasse.jpg" alt="img2-terrasseG"></a>
+            <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img3-terrasseG.jpg" data-lightbox="mygallery"><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img3-terrasse.jpg" alt="img3-terrasseG"></a>
+            <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img4-terrasseG.jpg" data-lightbox="mygallery"><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img4-terrasse.jpg" alt="img4-terrasseG"></a>
+        </div>
 
-<h3>Images piscine</h3>
-<div class="galerie">
-    <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img1-piscineG.jpg" data-lightbox="mygallery"><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img1-piscine.jpg" alt="img1-piscineG"></a>
-    <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img2-piscineG.jpg" data-lightbox="mygallery"><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img2-piscine.jpg" alt="img2-piscineG"></a>
-    <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img3-piscineG.jpg" data-lightbox="mygallery"><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img3-piscine.jpg" alt="img3-piscineG"></a>
-    <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img4-piscineG.jpg" data-lightbox="mygallery"><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img4-piscine.jpg" alt="img4-piscineG"></a>
-</div>
-
+        <h3>Images piscine</h3>
+        <div class="galerie">
+            <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img1-piscineG.jpg" data-lightbox="mygallery"><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img1-piscine.jpg" alt="img1-piscineG"></a>
+            <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img2-piscineG.jpg" data-lightbox="mygallery"><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img2-piscine.jpg" alt="img2-piscineG"></a>
+            <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img3-piscineG.jpg" data-lightbox="mygallery"><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img3-piscine.jpg" alt="img3-piscineG"></a>
+            <a href="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img4-piscineG.jpg" data-lightbox="mygallery"><img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/img4-piscine.jpg" alt="img4-piscineG"></a>
+        </div>
 
         <p>Que pensent nos clients de leur sol en résine ?</p>
-        <!-- partie carrousel-->
+            <!-- partie carrousel-->
         <div class="container">
             <div class="slide-container active">
                 <div class="slide">
@@ -87,7 +85,7 @@
                         <img src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/anonymous.jpg" alt="">
                         <div class="user-info">
                             <h3>Brice AG</h3>
-                            <div  class="stars">
+                            <div class="stars">
                                 <i class="fas fa-star"></i>
                                 <i class="fas fa-star"></i>
                                 <i class="fas fa-star"></i>
@@ -96,7 +94,6 @@
                             </div>
                         </div>
                     </div>
-                        
                     <p class="text">Projet mené au top avec un délai et une qualité tenus.
                     Loïc doit nous haïr pour le dessin…mais il aime relever des défis. Il est très doué. Merci à tous.</p>
                 </div>
@@ -126,7 +123,6 @@
             <div id="prev" class="fas fa-chevron-left" onclick="prev()"></div>
         </div>
         <!-- partie des réservations-->
-
         <h2>Un accompagnement personnalisé pour des projets sur-mesure</h2>
         <div class="presentation-severine">     
             <p class="presentation">
@@ -134,59 +130,54 @@
                 Je vous accompagne et vous <b>conseille</b> dans la conception de votre <b>terrasse</b>, de votre <b>plage de piscine</b> ou de votre <b>sol en résine</b> et en <b>granulat de marbre</b>. Nous prenons le temps de définir ensemble les <b>caractéristiques techniques</b>, les <b>coloris</b> et le <b>design</b>. Mon <b>intérêt</b> pour la <b>décoration</b> et l'aménagement d'espace me permet d'apporter ma touche déco et <b>féminine</b> à votre projet, pour qu'il vous corresponde et vous plaise dans la durée. Je m'investis dans chaque projet à vos côtés afin qu'il soit pensé pour <b>vos futurs moments de vie</b> et de partage !
             </p>
             <img id="presentation-img" src="http://localhost/projetStageWordpress/wp-content/themes/theme1_samuel/images/severine.jpg" alt="">
-            
         </div>
-        
-    </div>
+    </section>
 
-</section>
+    <section id="section3">
+        <div id="contact">
+            <h2>Vous avez un projet ? Terrasses, sols, allées, cours ; Prenons rendez-vous!</h2>
+            <p>Ensemble, nous concevons votre projet d'un point de vue technique et esthétique. Nous nous déplaçons et intervenons partout en Rhône-Alpes et Région PACA. Laissez-moi simplement vos coordonnées ou décrivez votre projet et je vous rappellerai !</p>
 
-<section id="section3">
-    <div id="contact">
-        <h2>Vous avez un projet ? Terrasses, sols, allées, cours ; Prenons rendez-vous!</h2>
-        <p>Ensemble, nous concevons votre projet d'un point de vue technique et esthétique. Nous nous déplaçons et intervenons partout en Rhône-Alpes et Région PACA. Laissez-moi simplement vos coordonnées ou décrivez votre projet et je vous rappellerai !</p>
+            <!-- Partie du formulaire -->
+            <form method="POST" action="front-page.php">
+                <h1>Contactez-nous</h1>
+                <div class="separation"></div>
+                <div class="corps-formulaire">
+                    <div class="gauche">
+                        <div class="groupe">
+                            <label>Objet </label>
+                            <input type="text" autocomplete="off" />
+                        </div>
+                        <div class="groupe">
+                            <label>Votre Nom</label>
+                            <input type="text" autocomplete="off" />
+                        </div>
+                        <div class="groupe">
+                            <label>Votre Prénom</label>
+                            <input type="text" autocomplete="off" />
+                        </div>
+                        <div class="groupe">
+                            <label>Votre adresse e-mail</label>
+                            <input type="text" autocomplete="off" />
+                        </div>
+                        <div class="groupe">
+                            <label>Votre téléphone</label>
+                            <input type="tel" autocomplete="off" />
+                        </div>
+                        
+                    </div>
 
-        <!-- Partie du formulaire -->
-        <form method="POST" action="front-page.php">
-            <h1>Contactez-nous</h1>
-            <div class="separation"></div>
-            <div class="corps-formulaire">
-                <div class="gauche">
-                    <div class="groupe">
-                        <label>Objet </label>
-                        <input type="text" autocomplete="off" />
-                    </div>
-                    <div class="groupe">
-                        <label>Votre Nom</label>
-                        <input type="text" autocomplete="off" />
-                    </div>
-                    <div class="groupe">
-                        <label>Votre Prénom</label>
-                        <input type="text" autocomplete="off" />
-                    </div>
-                    <div class="groupe">
-                        <label>Votre adresse e-mail</label>
-                        <input type="text" autocomplete="off" />
-                    </div>
-                    <div class="groupe">
-                        <label>Votre téléphone</label>
-                        <input type="tel" autocomplete="off" />
-                    </div>
-                    
-                </div>
-
-                <div class="droite">
-                    <div class="groupe">
-                        <label>Message</label>
-                        <textarea placeholder="Saisissez ici..."></textarea>
+                    <div class="droite">
+                        <div class="groupe">
+                            <label>Message</label>
+                            <textarea placeholder="Saisissez ici..."></textarea>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="pied-formulaire" align="center">
-                <button>Envoyer le message</button>
-            </div>
-        </form>
-        <p>Chez Terrasse Résine by Déconoble, nous vous accompagnons dans vos projets pour sublimer votre habitat et créer des espaces à votre image.</p>
-    </div>
-</section>
-
+                <div class="pied-formulaire" align="center">
+                    <button>Envoyer le message</button>
+                </div>
+            </form>
+            <p>Chez Terrasse Résine by Déconoble, nous vous accompagnons dans vos projets pour sublimer votre habitat et créer des espaces à votre image.</p>
+        </div>
+    </section>
